f2e0d324f10afeb1803d92f95568ca1d6f170858
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-07 16:23:07 -0700

    create database and role

--- snowflake_sql/create_role.sql
+++ snowflake_sql/create_role.sql
@@ -2,6 +2,6 @@ use role accountadmin;
 
 create warehouse dbt_warehouse with warehouse_size = 'x-small';
 
-create database dbt_db;
+create database if not exists dbt_db;
 
-create role dbt_role;
+create role if not exists dbt_role;
