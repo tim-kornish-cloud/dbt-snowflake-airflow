a6d9e07f137a6ba06430acb7bf43e8bb44b00998
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-07 17:10:34 -0700

    initialzed dbt creating a project data_pipeline

--- snowflake_sql/grants.sql
+++ snowflake_sql/grants.sql
@@ -1,5 +1,7 @@
 show grants on warehouse dbt_warehouse;
 
-grant usage on warehouse dbt_warehouse dbt_role;
+grant usage on warehouse dbt_warehouse to role dbt_role;
 
 grant all on database dbt_warehouse to role dbt_tole;
+
+GRANT ROLE dbt_role TO USER tlkornish;
