fe2c96a97deab5f4d38f160ca8c8549fc6264cd8
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-09-07 19:19:07 -0700

    adding sql to execute against snowflake

--- data_pipeline/models/staging/stg_tpch_orders.sql
+++ data_pipeline/models/staging/stg_tpch_orders.sql
@@ -1,1 +1,7 @@
-SELECT * FROM {{ source('tpch', 'orders') }}
+SELECT 
+	o_orderkey as order_key,
+	o_custkey as customer_key,
+	o_orderstatus as status_code,
+	o_totalprice as total_price,
+	o_orderdate as order_date
+FROM {{ source('tpch', 'orders') }}
